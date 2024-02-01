01b84706d93781c51eac659842d9b01cf2370972
Abhikrant <abhikrant.dhepe@gmail.com>
2024-02-02 00:43:40 +0530

    Sign In button Design
    Design changes of Sign In button

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -7,7 +7,7 @@ type Props = {}
 
 const SignInButton = (props: Props) => {
   return (
-    <Button className='font-semibold mr-2  text-lg' variant='ghost' onClick={()=>{
+    <Button className='font-semibold mr-2 bg-cyan-500 text-slate-950  hover:bg-cyan-600 rounded-xl text-lg' variant='ghost' onClick={()=>{
         signIn('google')
     }}>
         Sign In
